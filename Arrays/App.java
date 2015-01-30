9bb9ddf57662fd128f48d5306576423c13441d20
vivanovs <ivanovs223@gmail.com>
2015-01-30 11:42:26 +0000

    Updated code for test

--- Arrays/App.java
+++ Arrays/App.java
@@ -4,9 +4,10 @@ public class App {
 
 	public static void main(String[] args) {
 		int[] n = {1,2,4,5,6,7,8};
-		int[] m = new int[10];
+		int[] m = new int[4];
 		ArrayCopier ar = new ArrayCopier(n, m);
 		ar.run();
+		ar.printResult();
 		
 
 	}
