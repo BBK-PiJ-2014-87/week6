f416d09c5489ff7d305d5c185c9c412d972988fa
vivanovs <ivanovs223@gmail.com>
2015-01-30 11:40:54 +0000

    Added additional test cases

--- Arrays/TestArrayCopier.java
+++ Arrays/TestArrayCopier.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import recursionTest.PaperSize;
 
 public class TestArrayCopier {
 
@@ -12,11 +11,44 @@ public class TestArrayCopier {
 	public void testEqualArrays() {
 		int[] src = {22,33,44,55,66};
 		int[] dst = new int[5];
-		ArrayCopier ar = new AraryCopier(src, dst);
-		String input = "A00000";
-		int output = p.convertInputToSteps(input);
-		int expected = 4;
-		assertEquals(output, expected);
+		ArrayCopier ar = new ArrayCopier(src, dst);
+		ar.run();
+		int[] output = ar.getNewArray();
+		int[] expected = {22,33,44,55,66};
+		assertArrayEquals(output, expected);
+	}
+	
+	@Test
+	public void testOneSizeArray() {
+		int[] src = {22};
+		int[] dst = new int[1];
+		ArrayCopier ar = new ArrayCopier(src, dst);
+		ar.run();
+		int[] output = ar.getNewArray();
+		int[] expected = {22};
+		assertArrayEquals(output, expected);
+	}
+	
+	@Test
+	public void testLongerArray() {
+		int[] src = {1,2,3,4,5,6};
+		int[] dst = new int[4];
+		ArrayCopier ar = new ArrayCopier(src, dst);
+		ar.run();
+		int[] output = ar.getNewArray();
+		int[] expected = {1,2,3,4};
+		assertArrayEquals(output, expected);
+	}
+	
+	@Test
+	public void testShorterArray() {
+		int[] src = {1,2,3,4};
+		int[] dst = new int[6];
+		ArrayCopier ar = new ArrayCopier(src, dst);
+		ar.run();
+		int[] output = ar.getNewArray();
+		int[] expected = {1,2,3,4,0,0};
+		assertArrayEquals(output, expected);
 	}
 
 }
